b74b823c89fc149eb02384b4d61982e4b0d75a23
Eric Riotte <eriotte@gmail.com>
2013-12-16 20:12:37 -0500

    First commit. This is a rails app that is relying heavily on comfortable mexican sofa for content management

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Juicebox::Application.config.secret_key_base = '7931ee3e32e6f672aa1d466d95c084fea6558ae0f133b6dfccdb1b763ddc6ee506558fb950728743d5f43e2e912642384c084b50d658e48aefb018c930de9bde'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Juicebox::Application.config.session_store :cookie_store, key: '_juicebox_session'
